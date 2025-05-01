8d0c707d8e25a6e38035a8bcacfb51ff3a6a0f5c
Travis Hinkelman <thinkelman@esassoc.com>
2025-04-30 19:22:40 -0700

    Add prep of pop data and re-arrange/delete code

--- prep-data.R
+++ prep-data.R
@@ -1,14 +1,40 @@
 library(sf)
 library(dplyr)
-library(leaflet)
 
 if (!dir.exists("data")) dir.create("data")
+
+# Vehicle Population ------------------------------------------------------
 
 # https://www.energy.ca.gov/files/zev-and-infrastructure-stats-data
 ld_file = file.path("data", "Vehicle_Population.xlsx")
 if (!file.exists(ld_file)){
   download.file("https://www.energy.ca.gov/filebrowser/download/6311?fid=6311", ld_file)
 }
+
+ft_lev = c("Battery Electric (BEV)", "Plug-in Hybrid (PHEV)", "Fuel Cell (FCEV)", 
+           "Gasoline", "Gasoline Hybrid", "Diesel", "Other")
+
+counties_pop = readxl::read_xlsx(file.path("data", "Vehicle_Population.xlsx"),
+                                 sheet = "County") |> 
+  setNames(c("year", "county", "fuel_type_group", "fuel_type", "make", "model", "count")) |> 
+  filter(!(county %in% c("Out Of State", "Out of State"))) |> 
+  group_by(year, county, fuel_type = fuel_type_group) |> 
+  summarise(count = sum(count, na.rm = TRUE)) |> 
+  mutate(fuel_type = factor(fuel_type, levels = ft_lev))
+saveRDS(counties_pop, file.path("data", "counties_pop.rds"))
+
+zips_pop = readxl::read_xlsx(file.path("data", "Vehicle_Population.xlsx"),
+                             sheet = "ZIP") |> 
+  setNames(c("year", "fuel_type_group", "fuel_type", "zip", "count")) |> 
+  group_by(year, zip, fuel_type = fuel_type_group) |> 
+  summarise(count = sum(count, na.rm = TRUE)) |> 
+  mutate(zip = as.character(zip),
+         fuel_type = factor(fuel_type, levels = ft_lev)) 
+saveRDS(zips_pop, file.path("data", "zips_pop.rds"))
+
+file.remove(ld_file)
+
+# Spatial -----------------------------------------------------------------
 
 # https://gis.data.ca.gov/datasets/CALFIRE-Forestry::california-incorporated-cities-1/about
 cities = read_sf("https://egis.fire.ca.gov/arcgis/rest/services/FRAP/Incorp/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson") |> 
@@ -21,30 +47,6 @@ counties = read_sf("https://services3.arcgis.com/uknczv4rpevve42E/arcgis/rest/se
 # https://gis.data.ca.gov/datasets/CDEGIS::california-zip-codes/about
 zip_codes = read_sf("https://services3.arcgis.com/fdvHcZVgB2QSRNkL/arcgis/rest/services/ZipCodes/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")|> 
   st_make_valid()
-
-
-leaflet(options = leafletOptions(attributionControl = FALSE)) |>
-  setView(lng = -120, lat = 37.5, zoom = 6) |>
-  addProviderTiles(providers$Esri.WorldTopoMap, group = "Topo") |> 
-  addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") |> 
-  addLayersControl(baseGroups = c("Topo", "Satellite"),
-                   options = layersControlOptions(collapsed = FALSE)) |> 
-  addPolygons(data = counties,
-              label = ~CDTFA_COUNTY,
-              color = "black",
-              weight = 2,
-              fillOpacity = 0) |>
-  # addPolygons(data = filter(cities, CITY %in% tmp$CITY), 
-  #             label = ~CITY,
-  #             popup = ~COUNTY,
-  #             color = "black",
-  #             weight = 2,
-  #             fillOpacity = 0) |> 
-  addPolygons(data = zip_codes, 
-              label = ~ZIP_CODE,
-              popup = ~PO_NAME,
-              weight = 2,
-              fillOpacity = 0)
 
 # st_intersection for finding overlap is slow so using st_intersects first
 zip_county_inter = st_intersects(zip_codes, counties)
@@ -88,70 +90,42 @@ for (i in 1:nrow(counties)){
 }
 county_city = bind_rows(county_city_list)
 
-cities_sf = select(cities, objectid = OBJECTID, city = CITY, county = COUNTY)
-
-cities_file = file.path("data", "cities_sf.rds")
-if (!file.exists(cities_file)){
-  saveRDS(cities_sf, cities_file)
-}
+# cities_sf = select(cities, objectid = OBJECTID, city = CITY, county = COUNTY)
+# saveRDS(cities_sf, file.path("data", "cities_sf.rds"))
 
 counties_sf = counties |> 
   select(objectid = OBJECTID, county = CDT_NAME_SHORT, area_sqmi = AREA_SQMI) |> 
   left_join(county_city)
-
-counties_file = file.path("data", "counties_sf.rds")
-if (!file.exists(counties_file)){
-  saveRDS(counties_sf, counties_file)
-}
+saveRDS(counties_sf, file.path("data", "counties_sf.rds"))
 
 zips_sf = zip_codes |> 
   select(objectid = OBJECTID, zip = ZIP_CODE, area_sqmi = SQMI) |> 
   left_join(zip_county) |> 
   left_join(zip_city)
+saveRDS(zips_sf, file.path("data", "zips_sf.rds"))
 
-zips_file = file.path("data", "zips_sf.rds")
-if (!file.exists(zips_file)){
-  saveRDS(zips_sf, zips_file)
-}
+# library(leaflet)
+# leaflet(options = leafletOptions(attributionControl = FALSE)) |>
+#   setView(lng = -120, lat = 37.5, zoom = 6) |>
+#   addProviderTiles(providers$Esri.WorldTopoMap, group = "Topo") |> 
+#   addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") |> 
+#   addLayersControl(baseGroups = c("Topo", "Satellite"),
+#                    options = layersControlOptions(collapsed = FALSE)) |> 
+#   addPolygons(data = counties,
+#               label = ~CDTFA_COUNTY,
+#               color = "black",
+#               weight = 2,
+#               fillOpacity = 0) |>
+#   # addPolygons(data = filter(cities, CITY %in% tmp$CITY), 
+#   #             label = ~CITY,
+#   #             popup = ~COUNTY,
+#   #             color = "black",
+#   #             weight = 2,
+#   #             fillOpacity = 0) |> 
+#   addPolygons(data = zip_codes, 
+#               label = ~ZIP_CODE,
+#               popup = ~PO_NAME,
+#               weight = 2,
+#               fillOpacity = 0)
 
-# EDA ---------------------------------------------------------------------
 
-counties_pop = readxl::read_xlsx(file.path("data", "Vehicle_Population.xlsx"),
-                                 sheet = "County") |> 
-  setNames(c("year", "county", "fuel_type_group", "fuel_type", "make", "model", "count")) |> 
-  filter(!(county %in% c("Out Of State", "Out of State"))) 
-
-zips_pop = readxl::read_xlsx(file.path("data", "Vehicle_Population.xlsx"),
-                             sheet = "ZIP") |> 
-  setNames(c("year", "fuel_type_group", "fuel_type", "zip", "count")) |> 
-  mutate(zip = as.character(zip)) 
-
-# prop of vehicle count in zip codes that aren't represented in the spatial layer
-sum(zips_pop$count[is.na(zips_pop$prop_inc)])/sum(zips_pop$count)
-
-# compare county-level summary based on where spatial intersection occurred
-cp_summ = counties_pop |> 
-  group_by(county) |> 
-  summarise(count = sum(count, na.rm = TRUE)) |> 
-  left_join(counties_sf |> 
-              st_drop_geometry() |> 
-              select(county, area_sqmi, prop_inc)) |> 
-  mutate(inc_count_cp = count * prop_inc)
-
-zp_summ = zips_pop |> 
-  group_by(zip) |> 
-  summarise(count = sum(count, na.rm = TRUE)) |> 
-  left_join(zips_sf |> 
-              st_drop_geometry() |> 
-              select(zip, county, prop_inc)) |> 
-  mutate(inc_count_zp = count * prop_inc) |> 
-  group_by(county) |> 
-  summarise(inc_count_zp = sum(inc_count_zp, na.rm = TRUE))
-
-comp = left_join(zp_summ, cp_summ)
-
-library(ggplot2)
-
-ggplot(comp, aes(x = inc_count_zp, y = inc_count_cp)) +
-  geom_point() +
-  geom_abline(intercept = 0, slope = 1)
